bad8014bb2c8d7f0d9a55b2ba40094d4ecadb530
zhuangfan <fanzhuang@bipt.edu.cn>
2015-04-28 15:24:14 +0800

    Update triangle.js

--- triangle/triangle.js
+++ triangle/triangle.js
@@ -9,7 +9,6 @@ var triangle = {
     isIsosceles : function(a,b,c) {
     		if(a!=b&&b!=c&&a!=c) return false;
     		if(a+b<=c||a+c<=b||b+c<=a) return false ;
-    		if(a-b>=c||a-c>=b||b-c>=a) return false ;
     		if(a<=0) return false ;
     		if(b<=0) return false ;
     		if(c<=0) return false ;
@@ -17,7 +16,6 @@ var triangle = {
     },
     isScalene : function(a,b,c) {
 	    	if(a+b<=c||a+c<=b||b+c<=a)return false ;
-	    	if(a-b>=c||a-c>=b||b-c>=a)return false ;
 	    	if(a<=0) return false ;
 	    	if(b<=0) return false ;
 	    	if(c<=0) return false ;
