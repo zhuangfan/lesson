80d392b8507375b00432a89e1a89a1048648f1f7
zhuangfan <fanzhuang@bipt.edu.cn>
2015-05-25 18:53:27 +0800

    Update calcTest.js

--- calcUI/calcTest.js
+++ calcUI/calcTest.js
@@ -3,48 +3,47 @@ QUnit.test("It should handle key press", function( assert ) {
 	$('#2').click();
 	$('#3').click();
 	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 123 , "Passed!" );
+    assert.ok( num ==123 , "Passed!" );
 });
-QUnit.test("It should mutiply two numbers", function( assert ) {
+QUnit.test("It should handle key press", function( assert ) {
 	$('#1').click();
+	$('#1').click();
+	$('#dot').click();
+	$('#2').click();
+	$('#2').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num !=11.22 , "disPassed!" );
+});
+QUnit.test("It should divide two numbers1", function( assert ) {
+	$('#8').click();
+	$('#divide').click();
+	$('#4').click();
+	$('#equal').click();
+	num = calculatorUI.getDisplay() ;
+    assert.ok( num ==2 , "Passed!" );
+});
+QUnit.test("It should multiply two numbers1", function( assert ) {
+	$('#8').click();
 	$('#multiply').click();
 	$('#3').click();
 	$('#equal').click();
 	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 3 , "Passed!" );
-});
-QUnit.test("It should plus two numbers", function( assert ) {
-	$('#1').click();
-	$('#plus').click();
-	$('#3').click();
-	$('#equal').click();
-	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 3 , "Passed!" );
+    assert.ok( num ==24 , "Passed!" );
 });
 QUnit.test("It should minus two numbers", function( assert ) {
-	$('#1').click();
+	$('#9').click();
 	$('#minus').click();
-	$('#3').click();
+	$('#5').click();
 	$('#equal').click();
 	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 3 , "Passed!" );
+    assert.ok( num ==4 , "Passed!" );
 });
-QUnit.test("It should divide two numbers", function( assert ) {
-	$('#1').click();
-	$('#divide').click();
-	$('#3').click();
+QUnit.test("It should plus two numbers", function( assert ) {
+	$('#8').click();
+	$('#plus').click();
+	$('#9').click();
+	$('#8').click();
 	$('#equal').click();
 	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 3 , "Passed!" );
-});
-QUnit.test("It should C one numbers", function( assert ) {
-	$('#1').click();
-	$('#C').click();
-	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 1 , "Passed!" );
-});
-QUnit.test("It should AC all numbers", function( assert ) {
-	$('#AC').click();
-	num = calculatorUI.getDisplay() ;
-    assert.ok( num == 0 , "Passed!" );
+    assert.ok( num !=107 , "disPassed!" );
 });
